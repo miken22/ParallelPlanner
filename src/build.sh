04dc0b5a3af0b2bbcd279d77fecee0959928d9e5
Mike Nowicki <mikenowicki022@gmail.com>
2018-11-08 07:20:05 -0800

    Remove old src files, starting from scratch on this branch

--- src/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++0x -fopenmp -O3 -o Planner CG.cpp CGHeuristic.cpp CSP_Heuristic.cpp DTG.cpp Heuristic.cpp IterativeDeepening.cpp Launcher.cpp New_CGH.cpp Operator.cpp ParallelStacksID.cpp Planner.cpp Parser.cpp PIDA.cpp Search.cpp StrongConComp.cpp StateSpace.cpp SuccessorGenerator.cpp Timer.cpp Utilities.cpp Validate.cpp
